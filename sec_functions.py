008a82024eb4f49ab02691951a3b5a56861df734
Dmitrii Stepanov <dstepanov200498@gmail.com>
2023-05-30 00:48:46 +0400

    исправил ошибки

--- sec_functions.py
+++ sec_functions.py
@@ -1,11 +1,6 @@
 import hashlib
 import logging
-
-SETTING = {
-    'hash': '754a917a9c82f5247412006a5abe1c0eb76e1007',
-    'begin_digits': ['529460', '519998', '529025','5156451','522327','522329','523760','527652','528528','529158','529856','530176','531855','534133','518591','528154','526589','510144','532465','531456','531452','531963','534299','540989','530429','531866','534135','518640'],
-    'last_digits': '0758',
-}
+import json
 
 def check_hash(card_center: int, card_begin: int) -> int:
     """Функция делает проверку хэша по номеру карты
@@ -17,12 +12,12 @@ def check_hash(card_center: int, card_begin: int) -> int:
     Returns:
         int: номер, если проверка пройдена, и False в противном случае
     """
+    with open("setting.json", "r") as read_file:
+        SETTING = json.load(read_file)
     logging.info("Проверка хеша")
     card_number = str(card_begin) + str(card_center) + SETTING['last_digits']
-    card_hash = hashlib.sha1(card_number.encode()).hexdigest()
-    if SETTING['hash'] == card_hash:
-        return card_number
-    return False
+    card_hash = hashlib.sha1((card_number).encode()).hexdigest()
+    return card_number if SETTING['hash'] == card_hash else False
 
 
 def algorithm_luna(card_number: str) -> bool:
@@ -45,8 +40,5 @@ def algorithm_luna(card_number: str) -> bool:
             all_number[i] = tmp
     total_sum = sum(all_number)
     rem = total_sum % 10
-    if rem != 0:
-        check_sum = 10 - rem
-    else:
-        check_sum = 0
+    check_sum = 10 - rem if rem != 0 else 0
     return True if check_sum == check else False
